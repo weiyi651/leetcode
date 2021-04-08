96914104ea76316af6fada31f4f739980a42b570
weiyi <lwyhitwh@163.com>
2021-04-08 10:12:57 +0800

    palindrome linked list

--- moveZeros.py
+++ moveZeros.py
@@ -12,6 +12,8 @@ class Solution:
         left = right = 0
         while right < n:
             if nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
+                # 交换后，左指针一定不为0，右指针可能为0
+                nums[left], nums[right] = nums[right], nums[left] 
                 left += 1
+            # 出现一个0，右指针就比左指针多一位，即右指针和左指针的间隔是0的个数
             right += 1
--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-if __name__ == '__main__':
-    n = 6
-    print(Solution.climbStairs(Solution(), n))
